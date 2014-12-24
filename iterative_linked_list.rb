925d3b9faa9f1110745a4566a2a5eca544fc4f97
Lori Culberson <bigskybandit@sbcglobal.net>
2014-12-24 13:34:01 -0600

    first commit

---
+++ iterative_linked_list.rb
@@ -0,0 +1,24 @@
+# class Node
+#   attr_accessor :word, :next_node
+
+#   def initialize(word, next_node)
+#     @word = word
+#     @next_node = next_node
+#   end
+# end
+
+
+class IterativeLinkedList
+
+  def initialize
+    @head = nil
+  end
+
+  def count
+    0
+  end
+
+  def push(word)
+    
+  end
+end
